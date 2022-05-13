97056a6e4be4a486a5ea3ba2163aa60e445b921b
DrRedyx <dr.redyx@gmail.com>
2022-05-13 07:49:18 +0300

    Initial commit

---
+++ src/test/java/com/example/sky/pro/Telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sky.pro.Telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
